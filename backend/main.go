41c2e33a34614118e4e233ee37a21c20178f9b40
Papon <papon@isa3-dhcp-190-227.naist.jp>
2024-01-24 20:40:21 +0900

    can

--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,43 @@
-package main
+package main 
 
-import ("net/http";"fmt")
+import (
+		"log"
+		"net/http"
+		"fmt"
+		"encoding/json"
+		)
 		
+
+type Food_set struct{
+	Id int
+	Name string
+	Meal string
+	Stock int
+	Price float64
+}
+
+var Menu []Food_set
+
 func homepage( w http.ResponseWriter, r *http.Request){
+	log.Println("Endpoint Hit: Homepage")
 	fmt.Fprintf(w, "Hello from can")
-	fmt.Println("Homepage")
+}
+
+func returnAllmenu( w http.ResponseWriter, r *http.Request){
+	log.Println("Endpoint Hit: return all menu")
+	json.NewEncoder(w).Encode(Menu)
+}
+
+func handleRequests(){
+	http.HandleFunc("/menu", returnAllmenu)
+	http.HandleFunc("/", homepage)
+	http.ListenAndServe("localhost:10000",nil)
 }
 
 func main(){
-	http.HandleFunc("/", homepage)
-	http.ListenAndServe("localhost:10000",nil)
-	// fmt.Println("Homepage")
+	Menu = []Food_set{
+		Food_set{Id:1,Name:"Steak Salmon",Meal:"lunch",Stock:20,Price:100.00},
+		Food_set{Id:2,Name:"Grill Pork",Meal:"dinner",Stock:20,Price:100.00},
+	}
+	handleRequests()
 }
