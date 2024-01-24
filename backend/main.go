76d7e3e1cbc13ea376a84c706d889e154c4a9ba0
Papon <papon@isa3-dhcp-190-227.naist.jp>
2024-01-24 18:30:17 +0900

    can

--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,14 @@
 package main
 
-// import ("net/http";"fmt")
-import ("fmt")
+import ("net/http";"fmt")
 		
-// func homepage( w http.ResponseWriter, r *http.Request){
-// 	fmt.Fprintf(w, "Hello from can")
-// 	fmt.Println("Homepage")
-// }
+func homepage( w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Hello from can")
+	fmt.Println("Homepage")
+}
 
 func main(){
-	// http.HandleFunc("/", homepage)
-	// http.ListenAndServe("localhost:10000",nil)
-	fmt.Println("Homepage")
+	http.HandleFunc("/", homepage)
+	http.ListenAndServe("localhost:10000",nil)
+	// fmt.Println("Homepage")
 }
