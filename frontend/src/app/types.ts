21517260299871bd4db42c5f492dd0a08b168119
yonekuramiki <mikiyonekura4040@gmail.com>
2024-02-15 18:14:00 +0900

    api change

--- frontend/src/app/types.ts
+++ frontend/src/app/types.ts
@@ -1,11 +1,11 @@
 export interface Menu {
-    // id: number
-    id: string
-    name: string
-    // meal: string
-    // detail: string
-    // stock: number
-    // price: number
-    // picture: string
-    // date: string
+    id: number
+    // id: string
+    // name: string
+    meal: string
+    detail: string
+    stock: number
+    price: number
+    picture: string
+    date: string
 }
